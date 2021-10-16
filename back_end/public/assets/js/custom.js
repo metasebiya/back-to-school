ef893ac17703b83f17220feacfa4a9f94873a645
metasebiya <39261359+metasebiya@users.noreply.github.com>
2021-10-15 20:10:59 -1100

    merging app

---
+++ back_end/public/assets/js/custom.js
@@ -0,0 +1,11 @@
+import AOS from "aos/dist/aos";
+(function () {
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
+})();
